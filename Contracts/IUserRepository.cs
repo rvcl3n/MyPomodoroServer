c90665b96a8db99e8d3b153d0ddfaf41b92e18ea
rvcl3n <d.rudenko.1996@gmail.com>
2020-07-20 18:59:30 +0300

    Added User repository

---
+++ Contracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(Guid UserId);
+        void CreateUser(User user);
+        void UpdateUser(User dbUser, User user);
+        void DeleteUser(User user);
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return FindAll()
+                .OrderBy(p => p.Email)
+                .ToList();
+        }
+
+        public User GetUserById(Guid userId)
+        {
+            return FindByCondition(user => user.Id.Equals(userId)).ToList()
+                    .FirstOrDefault();
+        }
+
+        public void CreateUser(User user)
+        {
+            Create(user);
+        }
+
+        public void UpdateUser(User dbUser, User user)
+        {
+            Update(user);
+        }
+
+        public void DeleteUser(User user)
+        {
+            Delete(user);
+        }
+    }
+}
