da6a2734e7e8711f2c0fe7b020d3fbf72cef3302
rvcl3n <d.rudenko.1996@gmail.com>
2020-07-27 18:54:03 +0300

    Added Swagger to the Project

--- MyPomodoroServer/Startup.cs
+++ MyPomodoroServer/Startup.cs
@@ -61,6 +61,8 @@ namespace MyPomodoroServer
             services.Configure<IISOptions>(options =>
             {
             });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,6 +77,14 @@ namespace MyPomodoroServer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyPomodoro API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
